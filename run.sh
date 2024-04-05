a2a5a4b8ee72dfd2dcaa1f729addb1eb056cecb8
spkgyk <spkgyk@gmail.com>
2024-04-05 22:31:35 +0800

    change order#

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python train.py --model_name roberta-base
-python train.py --model_name facebook/bart-large
 python train.py --model_name distilbert/distilbert-base-uncased
+python train.py --model_name facebook/bart-large
+python train.py --model_name roberta-base
